85c0bf2fafea4b27c75b5e6a0caec5f979f183e5
icemonster <nunossabino@hotmail.com>
2020-01-28 01:30:44 +0000

    Fixed Ubuntu support. Updated bap to version 2.0.0

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # installation script for AutoVulnDetect
 # supports? Debian-based (apt) and RHEL (yum) systems
@@ -9,18 +9,20 @@
 
 OS=$(grep "^NAME=" /etc/os-release | cut -d= -f2)
 
-if [[ ${OS} = "Ubuntu" || ${OS} = "Debian" ]]
+if [[ ${OS} = "\"Ubuntu\"" || ${OS} = "Debian" ]]
 then
     echo "Installing on apt system"
     sudo apt install gcc-multilib gdb git libssl-dev openssl python python3 python3-pip python3-dev wget
-    wget https://github.com/BinaryAnalysisPlatform/bap/releases/download/v1.6.0/{bap,libbap,libbap-dev}_1.6.0.deb
+    wget https://github.com/BinaryAnalysisPlatform/bap/releases/download/v2.0.0/{bap,libbap,libbap-dev}_2.0.0.deb
     sudo dpkg -i {bap,libbap,libbap-dev}_1.6.0.deb
+    rm *.deb
 elif [[ ${OS} = "Fedora" || ${OS} = "CentOS" || ${OS} = "RedHat" ]]
 then
     echo "Installing on RHEL system"
     sudo yum install gdb gdb-gdbserver git glibc-devel.i686 openssl openssl-devel openssl-libs python python3 python3-pip python3-devel wget
-    wget https://github.com/BinaryAnalysisPlatform/bap/releases/download/v1.6.0/{bap,libbap,libbap-dev}-1.6.0-2.x86_64.rpm
+    wget https://github.com/BinaryAnalysisPlatform/bap/releases/download/v2.0.0/{bap,libbap,libbap-dev}-2.0.0-2.x86_64.rpm
     sudo rpm -i {bap,libbap,libbap-dev}-1.6.0-2.x86_64.rpm
+    rm *.rpm
 else
     echo "System ${OS} not supported yet"
     exit 1
